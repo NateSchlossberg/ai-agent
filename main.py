7de6a0f87a7600c48ccb1b87ff33af6f9de3bfd3
Nate Schlossberg <sonofernda@gmail.com>
2026-04-30 21:03:44 -0700

    added in feedback loops to the model so the agent can now accomplish tasks

--- main.py
+++ main.py
@@ -38,27 +38,36 @@ def main():
     
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    res = generate_content(client, messages)
-    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
-    if res.function_calls:
-        function_results = []
-        for fc in res.function_calls:
-            function_result = call_function(fc, args.verbose)
-            if not function_result.parts:
-                raise Exception(f'No response from function {fc.name} (no parts property)')
-            if not function_result.parts[0].function_response:
-                raise Exception(f'No response from function {fc.name}')
-            function_results.append(function_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_result.parts[0].function_response.response}")
+
+    for _ in range(20): #Hard stop at 20 to prevent token-burning spin
+        res = generate_content(client, messages)
         
-            
-    else:
-        print(res.text)
+        if res.candidates:
+            messages.extend(map(lambda c: c.content, res.candidates))
+
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
+        if res.function_calls:
+            function_results = []
+            for fc in res.function_calls:
+                function_result = call_function(fc, args.verbose)
+                if not function_result.parts:
+                    raise Exception(f'No response from function {fc.name} (no parts property)')
+                if not function_result.parts[0].function_response:
+                    raise Exception(f'No response from function {fc.name}')
+                function_results.append(function_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_result.parts[0].function_response.response}")
+                messages.append(types.Content(role="user", parts=function_results))
+        else:
+            print(res.text)
+            return
+        
+    # If we got here, then we failed to generate a response.
+    print('Failed to find a result after running 20 loops')
+    exit(1)
 
 if __name__ == "__main__":
     main()
